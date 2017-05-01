942fd73fcfab46dec180c63fe9f040393cebd8e1
David Cordero <dcorderoramirez@gmail.com>
2017-05-01 15:44:04 +0200

    Create Public and Private folders

---
+++ SwissTransportPortableLibrary/Public/SwissTransport.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using SwissTransportPortableLibrary;
+using SwissTransportPortableLibrary.Models;
+using SwissTransportPortableLibrary.NetworkDTOs;
+using SwissTransportPortableLibrary.Transformers;
+
+namespace SwissTransportPortableLibrary
+{
+    public class SwissTransport
+    {
+        private ApiClient ApiClient { get; set; }
+
+        public SwissTransport()
+        {
+            ApiClient = new ApiClient();
+        }
+
+        public async Task<List<Location>> GetLocations(string locationName)
+        {
+            var listOfLocationsDTO = await ApiClient.HttpGet<ListOfLocationsDTO>(String.Format("locations?query={0}", locationName));
+            return LocationsDTOToLocation.Transform(listOfLocationsDTO);
+        }
+
+        public async Task<Stationboard> GetStationBoard(string stationId)
+        {
+            var stationboardDTO = await ApiClient.HttpGet<StationboardDTO>(String.Format("stationboard?station={0}", stationId));
+            return StationboardDTOToModel.Transform(stationboardDTO);
+        }
+
+    }
+}
